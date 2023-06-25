b88a36d6116025102ba8fd3e0ed12da8bb796381
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-06-25 06:26:06 +0000

    [UPDATE]: Refactor the code for better memory management

--- push.c
+++ push.c
@@ -3,21 +3,24 @@
 /**
  * push - pushes an element onto the stack
  * @stack: pointer to the stack pointer
- * @n: the value to be pushed
+ * @value: the value to be pushed
+ * @line_number: line number of the push operation
  *
  * Return: void
  */
-void push(stack_t **stack, int n)
+void push(stack_t **stack, int value, unsigned int line_number)
 {
 	stack_t *new_node = malloc(sizeof(stack_t));
+	(void)(line_number);
 
 	if (new_node == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
+		free_stack(stack);
 		exit(EXIT_FAILURE);
 	}
-	new_node->n = n;
-	new_node->prev =  NULL;
+	new_node->n = value;
+	new_node->prev = NULL;
 	new_node->next = *stack;
 	if (*stack != NULL)
 		(*stack)->prev = new_node;
