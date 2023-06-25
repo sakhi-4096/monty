90bb4cdc6983db49178cf121f4d6e5f0ed0ab178
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-06-25 13:43:02 +0000

    Modify function call 'swap'

--- interpreter.c
+++ interpreter.c
@@ -44,7 +44,7 @@ void interpreter(FILE *file)
 			else if (strcmp(opcode, "pop") == 0)
 				pop(&stack, line_number);
 			else if (strcmp(opcode, "swap") == 0)
-				swap(&stack);
+				swap(&stack, line_number);
 			else if (strcmp(opcode, "add") == 0)
 				add(&stack, line_number);
 			else if (strcmp(opcode, "nop") == 0)
