92c5da8298a0ca12efbaa76d02d6b064505a64a0
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-06-25 13:20:41 +0000

    Add additional arguments for the function call 'pop'

--- add.c
+++ add.c
@@ -6,13 +6,13 @@
  *
  * Return: void
  */
-void add(stack_t **stack)
+void add(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "Error: can't add, stack too short\n");
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	(*stack)->next->n += (*stack)->n;
-	pop(stack);
+	pop(stack, line_number);
 }
