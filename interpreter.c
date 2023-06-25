5e32240d9e391cb20e4da59de3fbe4f42f8fe95e
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-06-25 13:23:57 +0000

    [UPDATE]: Add additional arguments to function calls

--- interpreter.c
+++ interpreter.c
@@ -40,13 +40,13 @@ void interpreter(FILE *file)
 			else if (strcmp(opcode, "pall") == 0)
 				pall(&stack);
 			else if (strcmp(opcode, "pint") == 0)
-				pint(&stack);
+				pint(&stack, line_number);
 			else if (strcmp(opcode, "pop") == 0)
-				pop(&stack);
+				pop(&stack, line_number);
 			else if (strcmp(opcode, "swap") == 0)
 				swap(&stack);
 			else if (strcmp(opcode, "add") == 0)
-				add(&stack);
+				add(&stack, line_number);
 			else if (strcmp(opcode, "nop") == 0)
 				nop(&stack);
 			else if (strcmp(opcode, "sub") == 0)
